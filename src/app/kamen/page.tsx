86c83b8bf4dd0c2d28a1b00115c8ce39739bc7e8
조우식 <chousik01@gmail.com>
2024-08-20 18:16:04 +0900

    page kamen

--- src/app/kamen/page.tsx
+++ src/app/kamen/page.tsx
@@ -1,14 +1,33 @@
 import React from "react";
-import PageComponent from "@/components/PageComponent";
-import "../globals.css"
-const Kamen:React.FC = ()=>{
+import "../globals.css";
+
+const Kamen: React.FC = () => {
   return (
-    <div>
-      <h1>hello Kamen</h1>
-      <PageComponent/>
+    <div className="flex flex-col">
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/kIUCdbI7AAs?si=Xr5KBS5uzOzBHUMA"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+        className="m-5"
+      ></iframe>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/1axpeCiCw6U?si=ST-yagjLjstHhqm-"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+        className="m-5"
+      ></iframe>
     </div>
-  )
-}
+  );
+};
 
-
-export default Kamen
+export default Kamen;
