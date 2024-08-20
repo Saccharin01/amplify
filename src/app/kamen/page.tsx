024b71c5686ee77ab9d6abae487698ea4ece8c71
조우식 <chousik01@gmail.com>
2024-08-20 12:06:02 +0900

    feat | tailwind test

--- src/app/kamen/page.tsx
+++ src/app/kamen/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-
-
-
+import PageComponent from "@/components/PageComponent";
+import "../globals.css"
 const Kamen:React.FC = ()=>{
   return (
     <div>
       <h1>hello Kamen</h1>
+      <PageComponent/>
     </div>
   )
 }
