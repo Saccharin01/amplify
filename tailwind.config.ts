0ce5d026255b677617690bc0657e244e3e7fea2a
조우식 <chousik01@gmail.com>
2024-08-20 18:15:21 +0900

    custom bg add

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,8 @@ const config: Config = {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'custom-bg': "url('/images/illiakanbg.png')", // 이미지 경로를 설정합니다.
       },
     },
   },
