4aded7af286b0919ff29f5a5510fc9a06a5d4303
조우식 <chousik01@gmail.com>
2024-08-20 16:44:02 +0900

    static file build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  output: 'export', //정적 사이트 생성(Static Site Generation, SSG) 모드를 활성화하는 데 사용
+  distDir: 'build',  // 빌드 결과물을 저장할 디렉터리 지정
+};
+  
 export default nextConfig;
